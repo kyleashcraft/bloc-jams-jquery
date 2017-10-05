572d656f58db9f48aa7fc6a692578adcdbcdf8fc
Kyle <ashcraft.k@husky.neu.edu>
2017-10-04 22:12:43 -0700

    added volume and time slider functionality, wrote player method to reformat duration from seconds to M:SS format and implemented throughout

--- scripts/player-bar.js
+++ scripts/player-bar.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
     var nextSong = album.songs[nextSongIndex];
     player.playPause(nextSong);
   });
+
   $('button#previous').click(function(){
     if (player.playState !== 'playing') {return;}
 
@@ -23,4 +24,22 @@ $(document).ready(function() {
     var previousSong = album.songs[previousSongIndex];
     player.playPause(previousSong);
   });
+
+  $('#time-control input').on('input', function(event){
+    player.skipTo(event.target.value);
+  });
+
+  $('#volume-control input').on('input', function(event){
+    player.setVolume(event.target.value);
+  });
+
+
+  setInterval( () => {
+    const currentTime = player.getTime();
+    const duration = player.getDuration();
+    const percent = (currentTime / duration) * 100;
+    $('#time-control .current-time').text( player.prettyTime(currentTime) );
+    $('#time-control .total-time').text(player.prettyTime(duration) );
+    $('#time-control input').val(percent);
+  }, 1000);
 });
